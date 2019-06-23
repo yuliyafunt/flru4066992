bafa9939fd9a425223a9fd284f7bedb2a789f900
Aleksey Koshelev <aleksey.koshelev.git@gmail.com>
2019-06-23 19:13:47 +0400

    Draft model

--- src/main/java/com/model/Match.java
+++ src/main/java/com/model/Match.java
@@ -1,7 +1,14 @@
 package com.model;
 
+import java.time.LocalDateTime;
+
 public class Match {
 
+    private String league;
+    private Team firstTeam;
+    private Team secondTeam;
+    private LocalDateTime matchTime;
 
-    // TODO: create fields here
+
+
 }
---
+++ src/main/java/com/model/Team.java
@@ -0,0 +1,8 @@
+package com.model;
+
+public class Team {
+
+    private String name;
+    private int score;
+    private double coefficient;
+}
